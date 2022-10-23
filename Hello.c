7ebcd25660fb5dbb8e19f4f365231b90f7d4fff9
Juhi Dwivedi <91658716+Inconsequential-24@users.noreply.github.com>
2022-10-23 14:19:23 +0530

    minor changes
    Minor changes to make the program more user firendly.

--- Hello.c
+++ Hello.c
@@ -4,10 +4,20 @@
 int main()
 {
   char name[100];
+  char ans;
   clrscr();
   printf("Enter your name: ");
   gets(name);
   printf("Hello, %s!", name);
+  printf("How are you %s?", name);
+  printf("Are you Fine? (Write Y-Yes or N-No)");
+  scanf("%c", &ans);
+  if (ans=='Y')
+  {
+    printf("You are Amazing! It's just a bad day not a bad life! We got your back! Stay Blessed!");
+  }
+  else
+    printf("We are glad that you are fine! Have an amazing Day!");
   getch();
   return 0;
 }
